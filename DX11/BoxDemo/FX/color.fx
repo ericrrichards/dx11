dfdd91849346947001c3479d9ddb7bc2429851aa
Eric Richards <ericrrichards@gmail.com>
2013-07-19 08:49:50 -0400

    stuff that was pulled

--- DX11/BoxDemo/FX/color.fx
+++ DX11/BoxDemo/FX/color.fx
@@ -26,8 +26,8 @@ float4 PS(VertexOut pin) :SV_Target {
 
 technique11 ColorTech {
 	pass P0{
-		SetVertexShader( CompileShader( vs_4_0, VS()));
+		SetVertexShader( CompileShader( vs_5_0, VS()));
 		SetGeometryShader(NULL);
-		SetPixelShader(CompileShader( ps_4_0, PS()));
+		SetPixelShader(CompileShader( ps_5_0, PS()));
 	}
 }
---
+++ DX11/ShapesDemo/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlimDX.Direct3D11;
+
+namespace ShapesDemo {
+    using System.Windows.Forms;
+
+    using Core;
+
+    using SlimDX;
+
+    using Buffer = SlimDX.Direct3D11.Buffer;
+
+    class ShapesDemo : D3DApp {
+        private Buffer _vb;
+        private Buffer _ib;
+
+        private Effect _fx;
+        private EffectTechnique _tech;
+        private EffectMatrixVariable _fxWVP;
+
+        private InputLayout _inputLayout;
+
+        private RasterizerState _wireframeRS;
+        private Matrix[] _sphereWorld = new Matrix[10];
+        private Matrix[] _cylWorld = new Matrix[10];
+        private Matrix _boxWorld;
+        private Matrix _gridWorld;
+        private Matrix _centerSphere;
+
+        private bool _disposed;
+        public ShapesDemo(IntPtr hInstance) : base(hInstance) {
+
+        }
+        protected override void Dispose(bool disposing) {
+            if (!_disposed) {
+                if (disposing) {
+                    
+                }
+                _disposed = true;
+            }
+            base.Dispose(disposing);
+        }
+        public override bool Init() {
+            return base.Init();
+        }
+        public override void OnResize() {
+            base.OnResize();
+        }
+        public override void UpdateScene(float dt) {
+            base.UpdateScene(dt);
+        }
+        public override void DrawScene() {
+            base.DrawScene();
+        }
+        protected override void OnMouseDown(object sender, MouseEventArgs mouseEventArgs) {
+            base.OnMouseDown(sender, mouseEventArgs);
+        }
+        protected override void OnMouseUp(object sender, MouseEventArgs e) {
+            base.OnMouseUp(sender, e);
+        }
+        protected override void OnMouseMove(object sender, MouseEventArgs e) {
+            base.OnMouseMove(sender, e);
+        }
+
+        private void BuildGeometryBuffers() {
+            
+        }
+        private void BuildFX() {
+            
+        }
+        private void BuildVertexLayout() {
+            
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
