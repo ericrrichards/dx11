6b850927c89f29569fe6240d67859a4ea91b46e7
Eric Richards <ericrrichards@gmail.com>
2013-08-07 10:03:30 -0400

    finished camera demo

--- DX11/BlendDemo/Program.cs
+++ DX11/BlendDemo/Program.cs
@@ -71,7 +71,7 @@ namespace BlendDemo {
             _radius = 80;
 
             MainWindowCaption = "Blend Demo";
-            Enable4XMsaa = false;
+            Enable4xMsaa = false;
 
             _landWorld = Matrix.Identity;
             _wavesWorld = Matrix.Translation(0, -3.0f, 0);
